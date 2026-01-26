8e257980e82c55c6da2dba5a7048fa0aaa9824cd
Stephen Gans, Jr. <104933279+gooseops@users.noreply.github.com>
2026-01-26 16:21:21 +0000

    fix: update flake.nix version

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {description = "Dev environment for core tech";
 
 inputs = {
-  nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
   nixpkgs-unst.url = "github:nixos/nixpkgs/nixos-unstable";
   flake-utils.url = "github:numtide/flake-utils";
 };
